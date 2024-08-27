7c009a534af72cba9bee0ddaf3aae7e8419efca8
ian <ianassah7@gmail.com>
2024-08-27 21:36:42 +0300

    first commit

---
+++ darkmode.js
@@ -0,0 +1,11 @@
+document.getElementById('mode-toggle').addEventListener('click', function() {
+  const body = document.body;
+  const navbar = document.querySelector('.navbar');
+  const footer = document.querySelector('footer');
+  body.classList.toggle('dark-mode');
+  navbar.classList.toggle('dark-mode');
+  footer.classList.toggle('dark-mode');
+
+  const isDarkMode = body.classList.contains('dark-mode');
+  this.textContent = isDarkMode ? '☀️' : '🌙';
+});
